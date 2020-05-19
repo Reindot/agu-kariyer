1e032e85ba2ec0fdaf8113b762891cf1a3bf4378
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-05-19 16:39:01 +0300

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:agucareer/locator.dart';
 import 'package:agucareer/pages/landing_page.dart';
+import 'package:agucareer/pages/login_page.dart';
 import 'package:agucareer/viewmodels/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+
+  //TEST
   setupLocator();
   runApp(MyApp());
 }
+
+
+//TEST
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,8 +21,14 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
         create: (context) => UserModel(),
         child: MaterialApp(
+
+
+          //TEST
           debugShowCheckedModeBanner: false,
-          home: LandingPage(),
+          home: LoginPage(),
         ));
   }
+
+
+  //TEST
 }
