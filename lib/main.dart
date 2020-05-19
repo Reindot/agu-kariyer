cf60fc472084ac194c89805836549d315ce0a0df
Ediz Mete <edizmetekurt@gmail.com>
2020-05-19 16:43:14 +0300

    final test

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:agucareer/locator.dart';
-import 'package:agucareer/pages/landing_page.dart';
 import 'package:agucareer/pages/login_page.dart';
 import 'package:agucareer/viewmodels/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-
-  //TEST
   setupLocator();
   runApp(MyApp());
 }
-
-
-//TEST
 
 class MyApp extends StatelessWidget {
   @override
@@ -21,14 +15,8 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
         create: (context) => UserModel(),
         child: MaterialApp(
-
-
-          //TEST
           debugShowCheckedModeBanner: false,
           home: LoginPage(),
         ));
   }
-
-
-  //TEST
 }
