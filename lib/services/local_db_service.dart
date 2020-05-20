8c3890c71c2cbd4ed6cade56866c11a39f443463
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-05-20 17:06:43 +0300

    save messages to local database

--- lib/services/local_db_service.dart
+++ lib/services/local_db_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:agucareer/models/message_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -18,29 +19,32 @@ class LocalDBService {
   initDb() async {
     var databasesPath = await getDatabasesPath();
     String path = databasesPath + '\\local.db';
-    debugPrint("dp createdd >>>>>> " +  path);
+    debugPrint("dp createdd >>>>>> " + path);
     return await openDatabase(path, onCreate: _onCreate, version: 1);
   }
 
   Future<FutureOr<void>> _onCreate(Database db, int version) async {
-
-    await db.execute(
-        "CREATE TABLE MESSAGES "
-            "(receiver TEXT PRIMARY KEY, "
-            "date TEXT, "
-            "message TEXT, "
-            "isMine TEXT, "
-            "on_db TEXT);");
-
-
+    await db.execute("CREATE TABLE MESSAGES "
+        "(id INTEGER PRIMARY KEY,"
+        "receiver TEXT, "
+        "date TEXT, "
+        "message TEXT, "
+        "isMine TEXT, "
+        "on_db TEXT);");
   }
 
-
-  void getMessages() async{
+  void getMessages() async {
     var dbClient = await db;
-    var result =  await dbClient.query("MESSAGES", orderBy: "date").asStream();
-
+    var result = await dbClient.query("MESSAGES", orderBy: "date").asStream();
   }
 
+  Future<int> sendMessage(Message message) async {
+    var dbClient = await db;
+    int id1 = await dbClient.rawInsert(
+        'INSERT INTO MESSAGES(receiver, date, message, isMine, on_db) '
+            'VALUES("${message.receiver}", "${message.date}", "${message.message}", "${message.isMine}", "false")');
+    debugPrint("insterted into db >>>>>>>>$id1" );
+    return id1;
 
+  }
 }
