73465083560a11379da4450d354cbc21c20fe097
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-05-20 15:44:29 +0300

    LocalDBService added

--- lib/locator.dart
+++ lib/locator.dart
@@ -2,6 +2,7 @@ import 'package:agucareer/repository/user_repository.dart';
 import 'package:agucareer/services/fireabase_storage_service.dart';
 import 'package:agucareer/services/firebase_auth_service.dart';
 import 'package:agucareer/services/firestore_db_service.dart';
+import 'package:agucareer/services/local_db_service.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
@@ -10,5 +11,6 @@ void setupLocator(){
   locator.registerLazySingleton(() => FirebaseAuthService());
   locator.registerLazySingleton(() => FirestoreDBService());
   locator.registerLazySingleton(() => FirebaseStorageService());
+  locator.registerLazySingleton(() => LocaleDBService());
   locator.registerLazySingleton(() => UserRepository());
 }
