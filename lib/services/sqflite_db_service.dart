c060c1e66ab3deb517f7b468ea1bd21fb56977bd
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-05-20 15:38:56 +0300

    local database building stage

--- lib/services/sqflite_db_service.dart
+++ lib/services/sqflite_db_service.dart
@@ -3,10 +3,10 @@ import 'dart:async';
 import 'package:agucareer/models/chats_model.dart';
 import 'package:agucareer/models/message_model.dart';
 import 'package:agucareer/models/user_model.dart';
-import 'package:agucareer/services/db_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
 
-class SqfliteDBService implements DBService {
+class SqfliteDBService {
   static Database _db;
 
   Future<Database> get db async {
@@ -19,71 +19,31 @@ class SqfliteDBService implements DBService {
   }
 
   initDb() async {
-    var dbFolder = await getDatabasesPath();
-    String path = dbFolder + "\\local.db";
+    var databasesPath = await getDatabasesPath();
+    String path = databasesPath + '\\demo.db';
+    debugPrint("dp createdd >>>>>> " +  path);
     return await openDatabase(path, onCreate: _onCreate, version: 1);
   }
 
   Future<FutureOr<void>> _onCreate(Database db, int version) async {
     await db.execute(
-        "CREATE TABLE MESSAGES(id INTEGER PRIMARY KEY, receiver INTEGER, message TEXT)");
+        "CREATE TABLE MESSAGES"
+            "(to TEXT PRIMARY KEY, "
+            "date TIMESTAMP, "
+            "message TEXT, "
+            "isMine TEXT, "
+            "on_db TEXT)");
   }
 
   /*
-  Future<List<Chats>> getChats() async{
-    var dbClient = await db;
-    var result = await dbClient.query("MESSAGES");
-    debugPrint("adadfd" + result.toString());
-    return null;
-  }
-  */
-
   @override
   Stream<List<Message>> getMessages(String fromID, String toID) {
-    // TODO: implement getMessages
-    return null;
-  }
+    var dbClient = db;
+    var result =  dbClient.query("MESSAGES", orderBy: "date").asStream();
+    return result.map((messageList) =>
+        messageList.documents.map((m) => Message.fromMap(m.data)).toList());
 
-  @override
-  Future<DateTime> getTime(String userID) {
-    // TODO: implement getTime
-    return null;
   }
-
-  @override
-  Future<User> getUser(String userID) {
-    // TODO: implement getUser
-    return null;
-  }
-
-  @override
-  Future<List<User>> getUserList() {
-    // TODO: implement getUserList
-    return null;
-  }
-
-  @override
-  Future<bool> saveUser(User user) {
-    // TODO: implement saveUser
-    return null;
-  }
-
-  @override
-  Future<bool> sendMessage(Message message) {
-    // TODO: implement sendMessage
-    return null;
-  }
-
-  @override
-  Future<bool> updateUser(String userID, Map<String, dynamic> map) {
-    // TODO: implement updateUser
-    return null;
-  }
-
-  @override
-  Future<List<Chats>> getChats(String userID) {
-    // TODO: implement getChats
-    return null;
-  }
+  */
 
 }
