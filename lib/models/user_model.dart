404fbc98b9ebaf8fd02d43d158438a3182ba4b63
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-05-24 23:54:49 +0300

    mentorID and modID added for User object

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -11,6 +11,8 @@ class User {
   String bio;
   String professional;
   String company;
+  String mentorID;
+  String modID;
 
   User({@required this.userID, @required this.email});
 
@@ -19,11 +21,14 @@ class User {
       'userID': userID,
       'email' : email,
       'profileURL' :profileURL ?? 'https://firebasestorage.googleapis.com/v0/b/agucareer-01.appspot.com/o/profile-photos%2Fdefault.jpg?alt=media&token=7b51d2e6-5e86-45ba-b6a3-885cbff8205e',
-      'type' : type ?? 'Type',
-      'name' : name ?? 'Default Name',
-      'bio' : bio ?? 'Bio',
-      'professional' : professional ?? 'Professional',
-      'company' : company ?? 'Company',
+      'type' : type ?? '',
+      'name' : name ?? '',
+      'bio' : bio ?? '',
+      'professional' : professional ?? '',
+      'company' : company ?? '',
+      'mentorID' : mentorID ?? '',
+      'modID' : modID ?? '',
+
     };
   }
 
@@ -35,11 +40,13 @@ class User {
         name = map['name'],
         bio = map['bio'],
         professional = map['professional'],
+        mentorID = map['mentorID'],
+        modID = map['modID'],
         company = map['company'];
 
   @override
   String toString() {
-    return 'User{userID: $userID, email: $email, profileURL: $profileURL, type: $type, name: $name, bio: $bio, professional: $professional, company: $company}';
+    return 'User{userID: $userID, email: $email, profileURL: $profileURL, type: $type, name: $name, bio: $bio, professional: $professional, company: $company, mentorID: $mentorID, modID: $modID}';
   }
 
 }
